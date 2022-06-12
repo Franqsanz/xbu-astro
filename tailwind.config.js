73c3261b8470b501b5e2ef5d05c549ddb518292f
franco sanchez <franco_genta@hotmail.com>
2022-06-12 11:18:51 -0300

    update: dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,31 @@
 const colors = require('tailwindcss/colors');
 
-module.exports = {
-  mode: 'jit',
-  purge: [
+module.exports = { 
+  content: [
     './public/**/*.html',
     './src/**/*.{astro,js,svelte,ts}'
   ],
   theme: {
-    fontFamily: {
-      poppins: 'Poppins, sans-serif',
-    },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      white: colors.white,
-      black: colors.black,
-      gray: colors.trueGray,
-      red: colors.red,
-      blue: colors.sky,
-      yellow: colors.amber,
-      orange: colors.orange,
-      cyan: colors.cyan,
-      lime: colors.lime,
-      green: colors.green,
-      indigo: colors.indigo,
+    extend: {
+      // darkMode: 'class', 
+      fontFamily: {
+        poppins: 'Poppins, sans-serif',
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: colors.white,
+        black: colors.black,
+        gray: colors.neutral,
+        red: colors.red,
+        blue: colors.sky,
+        yellow: colors.amber,
+        orange: colors.orange,
+        cyan: colors.cyan,
+        lime: colors.lime,
+        green: colors.green,
+        indigo: colors.indigo,
+      }
     }
   },
   plugins: [
